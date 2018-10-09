f69e3ebba4b46976e1b7f8c44f74a9e3443e9e50
Alex <sasha.satsunkevich@gmail.com>
2018-10-09 22:17:36 +0300

    start work with game, create main project, add Ioc folder and fil it

---
+++ MiniRPG/MiniRPG/IoC/Resolver.cs
@@ -0,0 +1,24 @@
+﻿using StructureMap;
+
+namespace MiniRPG.IoC
+{
+    internal static class Resolver
+    {
+        private static IContainer _container;
+
+        public static IContainer Current
+        {
+            get
+            {
+                if (_container == null)
+                    _container = Initialize();
+                return _container;
+            }
+        }
+
+        private static IContainer Initialize()
+        {
+            return new Container(c => c.AddRegistry<DefaultRegistry>());
+        }
+    }
+}
