a19106b6dddab3bcd4a46a7c7adb5ea49797e66e
Aliaksandr_Satsunkevich <Aliaksandr_Satsunkevich@epam.com>
2018-10-15 14:35:18 +0300

    Add Node in GameAI

--- MiniRPG/Engine/AI/Node.cs
+++ MiniRPG/Engine/AI/Node.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Engine.Enums;
 
 namespace Engine.AI
 {
     class Node
     {
+        public ActionTypes Action { get; private set; }
+        public GameState State { get; private set; }
+
+        public Node()
+        { }
+
+        public Node(GameState state)
+        {
+            State = state;
+        }
+
+        public Node(GameState state, ActionTypes action)
+        {
+            State = state;
+            Action = action;
+        }
+
+        public int Win { get; set; }
+        public int Death { get; set; }
+        public int Health { get; set; }
+
+        public int Value
+        {
+            get
+            {
+                if (Death > Win)
+                    return int.MinValue;
+                return Win + Health - Death * 5;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Action, Value);
+        }
     }
 }
